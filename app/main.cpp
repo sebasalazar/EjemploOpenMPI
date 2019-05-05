ddc5ddd3dd89ba710db398d91ed2912ffe545f6d
aquinve <amada.quinterosv@utem.cl>
2019-05-05 17:10:54 -0400

    Control N° 2 Computación Paralela y Distribuida/ Se le agrega nombre de la máquina fecha y tiempo

--- app/main.cpp
+++ app/main.cpp
@@ -1,3 +1,9 @@
+//Control N°2
+//Amada Quinteros Véliz
+
+using namespace std;
+#include <ctime> /* Librería para obtener la fecha y tiempo */
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,17 +19,24 @@
  * Salida: contenido de los mensajes recibidos por 
  * el proceso 0, que los imprime
  */
+
 int main(int argc, char** argv) {
     int mi_rango; /* rango del proceso    */
     int p; /* numero de procesos   */
     int fuente; /* rango del que envia  */
     int dest; /* rango del que recibe */
     int tag = 0; /* etiqueta del mensaje */
+    int largo_n = 0; /* Largo nombre */
     char mensaje[100]; /* mensaje  */
+    char nombre_p[50]; /*Nombre de la máquina */
+
     MPI_Status estado; /* devuelve estado al recibir*/
 
     /* Comienza las llamadas a MPI */
     MPI_Init(&argc, &argv);
+
+    /* Obtener el nombre de la máquina */
+    MPI_Get_processor_name(nombre_p, &largo_n);
 
     /* Averiguamos el rango de nuestro proceso */
     MPI_Comm_rank(MPI_COMM_WORLD, &mi_rango);
@@ -31,11 +44,15 @@ int main(int argc, char** argv) {
     /* Averiguamos el número de procesos que estan 
      * ejecutando nuestro porgrama 
      */
+
+    time_t t = time(NULL); /* Variable para almacenar el tiempo de calendario */
+    struct tm tm = *localtime(&t); /* Estructura utilizada para mantener la hora y fecha */
+
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 
     if (mi_rango != 0) {
         /* Crea mensaje */
-        sprintf(mensaje, "Saludos del proceso %d!", mi_rango);
+        sprintf(mensaje,"%s %d-%d-%d %d:%d:%d", nombre_p, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
         dest = 0;
         /* Usa strlen+1 para que la marca /0 se transmita */
         MPI_Send(mensaje, strlen(mensaje) + 1, MPI_CHAR,
